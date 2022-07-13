00756d5218923e7b621b79b7e2edc654c5279437
ManoharGanganna <98412027+ManoharGanganna@users.noreply.github.com>
2022-07-13 11:58:17 +0100

    Update routes.py
    Changed according to logic

--- flask_youtube_search/routes.py
+++ flask_youtube_search/routes.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, current_app, request
+from flask import Blueprint, render_template, current_app, request, url_for, redirect, flash
 import requests
 from isodate import parse_duration
 from .videodownload import video_downloader
+from .audio_prediction import audio_pred
 import re
+import os
 
 main = Blueprint('main', __name__)
 
@@ -54,15 +56,42 @@ def index():
                 }
                 videos.append(video_data)
 
-        if 'download' in request.form:
-            download_url = request.form.get('download')
-            file_name_url = re.search(r"[^v=]*$", download_url)
-            file_name = file_name_url.group()
-            video_downloader(download_url, file_name)
-            return render_template('index.html')
+            if 'download' in request.form:
+                download_url = request.form.get('download')
+                thumbnail = request.form.get('thumbnail')
+                file_name_url = re.search(r"[^v=]*$", download_url)
+                file_name = file_name_url.group()
+                download_status = video_downloader(download_url, file_name)
+                if download_status == 'Download_complete':
+                    return redirect(url_for('.success', file=file_name, thumbnail=thumbnail))
+                else:
+                    message = f"The Video Download was unsuccessful due to {download_status}"
+                    flash(message)
+                    return render_template('index.html')
 
         return render_template('index.html', videos=videos)
 
     else:
 
         return render_template('index.html')
+
+
+@main.route('/success', methods=['GET', 'POST'])
+def success():
+    file = request.args.get("file")
+    thumbnail = request.args.get("thumbnail")
+    cwd = os.getcwd()
+    static_file_path = os.path.join(cwd, 'static')
+    audio_file_path = os.path.join(static_file_path, 'audios')
+    audio_file = file + '.mp3'
+    video_file = file + '.mp4'
+    audio_loc = os.path.join(audio_file_path, audio_file)
+    if request.method == 'POST':
+        res, res1 = audio_pred(audio_loc)
+        vd_file = 'videos/' + video_file
+        print('routes',res1)
+        res_dict = {'Voice': 'Duration'}
+        res_dict.update(res)
+        print('b', res_dict)
+        return render_template('piechart.html', data=res_dict)
+    return render_template('success.html')
