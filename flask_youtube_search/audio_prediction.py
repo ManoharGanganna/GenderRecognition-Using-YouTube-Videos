6dbf5ec95273cac073a86e7d70debb4c55e9a138
ManoharGanganna <98412027+ManoharGanganna@users.noreply.github.com>
2022-08-02 20:03:30 +0100

    Update audio_prediction.py
    Added changes of javascript

--- flask_youtube_search/audio_prediction.py
+++ flask_youtube_search/audio_prediction.py
@@ -3,10 +3,10 @@ from inaSpeechSegmenter import Segmenter
 
 
 def audio_pred(audio):
-    seg = Segmenter()
+    seg = Segmenter(ffmpeg='C:/Users/sairam/PycharmProjects/FFmpeg/bin/ffmpeg.exe')
     segmentation = seg(audio)
     initial_dict = dict()
-    print(segmentation)
+    js_input = list(map(list, segmentation))
     total_time = segmentation[-1][-1]
     voice_list = []
     percent_list = []
@@ -20,5 +20,7 @@ def audio_pred(audio):
     zipped = zip(voice_list, percent_list)
     voice_percent_lst = list(zipped)
     result_dict1 = dict(zip(initial_dict.keys(), [round(sum(item)) for item in initial_dict.values()]))
-    result_dict = {'No Audio' if k == 'noEnergy' else k: v for k, v in result_dict1.items()}
-    return result_dict, voice_percent_lst
+    result_dict = {'no audio' if k == 'noEnergy' else k: v for k, v in result_dict1.items()}
+    sorted_dict = {k: result_dict[k] for k in sorted(result_dict.keys())}
+    print(sorted_dict)
+    return sorted_dict, voice_percent_lst, js_input
