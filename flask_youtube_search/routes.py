a1d6de20e55b33c449032580777748e7755835cd
ManoharGanganna <98412027+ManoharGanganna@users.noreply.github.com>
2022-08-02 20:16:33 +0100

    Update routes.py
    Added logic to save videos from s3 bucket and access and play them from s3 bucket. Delete audio and video files from server after the results are processed.

--- flask_youtube_search/routes.py
+++ flask_youtube_search/routes.py
@@ -2,7 +2,6 @@ from threading import Thread
 from flask import Blueprint, render_template, current_app, request, url_for, redirect, flash
 import requests
 import re
-import os
 import ast
 from extensions import mysql
 from isodate import parse_duration
@@ -10,6 +9,15 @@ from .videodownload import video_downloader
 from .audio_prediction import audio_pred
 from .sendemail import send_email
 import json
+import boto3
+import os
+
+s3 = boto3.client('s3',
+                  aws_access_key_id='secretaccesskeyid',
+                  aws_secret_access_key='secretaccesskey'
+                  )
+
+BUCKET_NAME = 'flaskappvideos-genderprediction'
 
 main = Blueprint('main', __name__)
 
@@ -66,13 +74,26 @@ def index():
                 thumbnail = request.form.get('thumbnail')
                 file_name_url = re.search(r"[^v=]*$", download_url)
                 file_name = file_name_url.group()
-                download_status = video_downloader(download_url, file_name)
-                if download_status == 'Download_complete':
+                download_cur = mysql.connection.cursor()
+                download_value = download_cur.execute(
+                    "SELECT prediction_results,js_results FROM prediction_data WHERE video_id = %s ",
+                    (file_name,))
+                if download_value > 0:
                     return redirect(url_for('.success', file=file_name, thumbnail=thumbnail))
                 else:
-                    message = f"The Video Download was unsuccessful due to {download_status}"
-                    flash(message)
-                    return render_template('index.html')
+                    video_file = file_name + '.mp4'
+                    download_status = video_downloader(download_url, file_name)
+                    if download_status == 'Download_complete':
+                        s3.upload_file(
+                            Bucket=BUCKET_NAME,
+                            Filename=video_file,
+                            Key=video_file
+                        )
+                        return redirect(url_for('.success', file=file_name, thumbnail=thumbnail))
+                    else:
+                        message = f"The Video Download was unsuccessful due to {download_status}"
+                        flash(message)
+                        return render_template('index.html')
 
         return render_template('index.html', videos=videos)
 
@@ -85,51 +106,75 @@ def process_results(app, audio_location, to_mail, file_name):
     result, result1, result2 = audio_pred(audio_location)
     res_dict = {'Voice': 'Duration'}
     res_dict.update(result)
+    jsr = json.dumps(result2)
+    ad_file = file_name + '.mp3'
     with app.app_context():
         insert_cur = mysql.connection.cursor()
-        insert_cur.execute("INSERT INTO prediction_data(video_id,prediction_results)VALUES(%s,%s)",
-                           (file_name, res_dict))
+        insert_cur.execute("INSERT INTO prediction_data(video_id,prediction_results,js_results)VALUES(%s,%s,%s)",
+                           (file_name, res_dict, jsr))
         mysql.connection.commit()
         insert_cur.close()
         send_email(to_mail)
+        file_exists = os.path.exists(ad_file)
+        if file_exists:
+            os.remove(ad_file)
+
+
+def show_video(bucket, videokey):
+    try:
+        presigned_url = s3.generate_presigned_url('get_object',
+                                                  Params={'Bucket': bucket, 'Key': videokey},
+                                                  ExpiresIn=3600)
+    except Exception as e:
+        pass
+    return presigned_url
 
 
 @main.route('/success', methods=['GET', 'POST'])
 def success():
     file = request.args.get("file")
     thumbnail = request.args.get("thumbnail")
-    cwd = os.getcwd()
-    static_file_path = os.path.join(cwd, 'static')
-    audio_file_path = os.path.join(static_file_path, 'audios')
     audio_file = file + '.mp3'
     video_file = file + '.mp4'
-    vd_file = 'videos/' + video_file
-    audio_loc = os.path.join(audio_file_path, audio_file)
     if request.method == 'POST':
         if 'results' in request.form:
+            file_exists = os.path.exists(video_file)
+            if file_exists:
+                os.remove(video_file)
             retrieval_cur = mysql.connection.cursor()
-            resultvalue = retrieval_cur.execute("SELECT prediction_results,js_results FROM prediction_data WHERE video_id = %s ",
-                                                (file,))
+            resultvalue = retrieval_cur.execute(
+                "SELECT prediction_results,js_results FROM prediction_data WHERE video_id = %s ",
+                (file,))
             if resultvalue > 0:
                 calculated_result = retrieval_cur.fetchone()
                 voice_dict = calculated_result[0]
                 converteddict = ast.literal_eval(voice_dict)
                 jr = calculated_result[1]
-                return render_template('videoplayer.html', video_id=vd_file, data=converteddict, js_ip=jr)
+                contents = show_video(BUCKET_NAME, video_file)
+                return render_template('videoplayer.html', video_id=contents, data=converteddict, js_ip=jr)
                 retrieval_cur.close()
             else:
-                res, res1, res2 = audio_pred(audio_loc)
+                res, res1, res2 = audio_pred(audio_file)
                 jsr = json.dumps(res2)
                 res_dict = {'Voice': 'Duration'}
+                print(res)
                 res_dict.update(res)
                 insert_cur = mysql.connection.cursor()
-                insert_cur.execute("INSERT INTO prediction_data(video_id,prediction_results,js_results)VALUES(%s,%s,%s)",
-                                   (file, res_dict, jsr))
+                insert_cur.execute(
+                    "INSERT INTO prediction_data(video_id,prediction_results,js_results)VALUES(%s,%s,%s)",
+                    (file, res_dict, jsr))
                 mysql.connection.commit()
                 insert_cur.close()
-                return render_template('videoplayer.html', video_id=vd_file, data=res_dict, js_ip=json.dumps(res2))
+                file_exists = os.path.exists(audio_file)
+                if file_exists:
+                    os.remove(audio_file)
+                contents = show_video(BUCKET_NAME, video_file)
+                return render_template('videoplayer.html', video_id=contents, data=res_dict, js_ip=json.dumps(res2))
         elif 'email' in request.form:
             email = request.form.get('email')
+            file_exists = os.path.exists(video_file)
+            if file_exists:
+                os.remove(video_file)
             if email is not None:
                 retrieval_cur_email = mysql.connection.cursor()
                 resultvalue = retrieval_cur_email.execute(
@@ -140,7 +185,7 @@ def success():
                 else:
                     flash("Email will be sent after Results are processed")
                     app = current_app._get_current_object()
-                    Thread(target=process_results, args=(app, audio_loc, email, file,)).start()
+                    Thread(target=process_results, args=(app, audio_file, email, file,)).start()
                     return render_template('success.html')
 
     return render_template('success.html')
